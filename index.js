a22c93e840a982773cac5faadbfd4f437b71c684
Abayanathan Radhakrishnan <abradhak@abradhak-mac.attlocal.net>
2021-09-20 17:56:51 -0400

    auth added to post routine

--- index.js
+++ index.js
@@ -14,8 +14,12 @@ app.get('/login', (req, res) => {
 });
 
 app.post('/login', (req, res) => {
+  var authHeader = req.headers.authorization;
+  var auth = new Buffer.from(authHeader.split(' ')[1], 'base64').toString().split(':');
+  var user = auth[0];
+  var pass = auth[1];
   res.setHeader('Content-Type', 'application/json');
-  res.status(200).send(JSON.stringify({sub:"abayadmin", name:"admintest"}));
+  res.status(200).send(JSON.stringify({sub:"abayadmin", name:"admintest",user:user,password:pass }));
 });
 
 const port = process.env.PORT || 8080;
